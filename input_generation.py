4e0931133ef1eefca1f7156fe645d21157447401
cmdoret <cyril.matthey-doret@unil.ch>
2019-02-15 03:43:30 +0100

    add generation of SVs and edition of fasta files

--- input_generation.py
+++ input_generation.py
@@ -3,6 +3,7 @@
 
 import numba as nb
 import numpy as np
+from Bio import SeqIO
 
 
 @nb.njit(error_model='numpy', parallel=True)
@@ -57,3 +58,78 @@ def subset_mat(matrix, coords, labels, winsize=128):
         y[i] = labels[i]
 
     return out
+
+
+def edit_genome(fasta_in, fasta_out, sv_dict):
+    """
+    Given a fasta file and a dictionary of structural variants and their
+    positions, generate a new genome by applying the input changes.
+    Parameters
+    ----------
+    fasta_in : str
+        Path to the input genome in fasta format.
+    fasta_out : str
+        Path where the edited genome will be written in fasta format.
+    sv_dict : dict
+        Dictionary with names of structural variants as keys and tuples
+        of chromosome names and coordinates as values. Currently, the only
+        structural variants supported are inversions.
+    Returns
+    -------
+    """
+    with open(fasta_out, 'w') as fa_out:
+        for chrom in SeqIO.parse(fasta_in, format='fasta'):
+            for sv_type, coords in sv_dict:
+                if sv_type == "INV":
+                    for sv in coords:
+                        start, end = sv[1]
+                        if sv[0] == chrom.id:
+                            chrom.seq[start: end] = chrom.seq[end:start]
+        SeqIO.write(chrom, fa_out)
+
+
+def generate_sv(
+        chromsizes,
+        sv_freqs={"INV": 0.01},
+        params={"INV": 2250, "DEL": 3100},
+                ):
+    """
+    Generates random structural variations, given a dictionary of SV types
+    and their frequencies, as well as a dictionary of chromosome names and
+    their respective lengths.
+    Parameters
+    ----------
+    chromsizes : dict
+        Dictionary with chromosome names as keys and their length in basepairs
+        as values.
+    sv_freqs : dict
+        Dictionary with event types as keys (such as INV, DEL, INS, TRA)
+        and their frequencies, in #event / kb.
+    params : dict
+        Dictionary with SV types as keys and their characteristics as values.
+    For inversions and deletions, mean length is used. (chosen
+    approximately based on Sudmant et al, Science, 2015).
+    Returns
+    -------
+    list :
+        A list where each element is an SV. Each SV is a tuple of
+        (chrom, (coords)).
+    """
+    # Relative abundance of each event type (placeholder values)
+    rel_abun = {"INV": 8, "DEL": 400, "DUP": 60, "INS": 160, "CNV": 350}
+    for chrom, size in chromsizes:
+        n_sv = size * sum(sv_freqs.values()) / 1000
+        out_sv = [None * n_sv]
+        sv_count = 0
+        for sv_type in sv_freqs:
+            prop_event = rel_abun[sv_type] / \
+                sum([n for s, n in rel_abun if s in sv_freqs])
+            n_event = n_sv * prop_event
+            for _ in n_event:
+                start = np.random.randint(size)
+                end = start + np.random.normal(loc=params[sv_type],
+                                               scale=0.1 * params[sv_type])
+                out_sv[sv_count] = (chrom, (start, end))
+                sv_count += 1
+
+    return out_sv
