00497277f319a09bbb4c9e06543fa934c65b7604
cmdoret <cyril.mattheydoret@gmail.com>
2021-02-02 22:29:50 +0100

    add visualization and preprocessing of inputs, additional dropout layers and convolution filters

--- hic_scrambler/model.py
+++ hic_scrambler/model.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 from sklearn.model_selection import StratifiedKFold
 from keras.models import model_from_json
+from keras.utils.vis_utils import plot_model
 import glob
 import re
 from os.path import join
@@ -34,10 +35,30 @@ def load_data(training_path="data/input/training"):
     x_data = np.vstack(list(x_data))
     y_data = np.hstack(list(y_data))
     x_data = tf.keras.utils.normalize(x_data, axis=1)
+    # Flatten Hi-C images: Divide each image by a diagonal gradient
+    # computed across whole dataset
+    x_data = erase_diags(x_data)
     # X must be 4D (for convolutional layers) and made up of floats
     x_data = x_data[:, :, :, None].astype(float)
     return x_data, y_data
 
+def erase_diags(imgs):
+    """
+    Given a stack of N images of shape x,y of shape (N,x,y), divide each
+    pixel of each image by the average of its diagonal across the whole stack
+    """
+    # Mean of each pixel across all imgs
+    avg_img = imgs.mean(axis=0)
+    # Mean of each diagonal across stack
+    avg_dia = [np.mean(np.diagonal(avg_img, d)) for d in range(avg_img.shape[0])]
+    # Build matrix as a smooth diagonal gradient
+    avg_grd = np.zeros(avg_img.shape)
+    for k, v in enumerate(avg_dia):
+        avg_grd += np.diag([v for i in range(avg_img.shape[0] - k)], k=k)
+    # Make gradient symmetric
+    avg_grd = avg_grd + np.transpose(avg_grd) - np.diag(np.diag(avg_grd))
+    # Divide each image in the stack by gradient
+    return imgs / avg_grd
 
 def create_model(img_size, n_labels, n_neurons=18):
     """Builds model from scratch for training"""
@@ -47,25 +68,23 @@ def create_model(img_size, n_labels, n_neurons=18):
     # Need to start w/ some conv layers to use neighbourhood info
     # conv2d(n_output_channels, kernel_size, ...)
     # 128x128 - (k-1) -> 126x126
-    model.add(tf.keras.layers.Conv2D(4, 3, activation="relu"))
-
+    model.add(tf.keras.layers.Conv2D(32, 3, activation="relu"))
     # Dropout to reduce overfitting
-    model.add(tf.keras.layers.Dropout(0.2))
-
-    # 126x126 / 2 -> 63x63
+    model.add(tf.keras.layers.Dropout(0.4))
+    # 126x126 / 2 -> 62x62x32
     model.add(tf.keras.layers.MaxPooling2D((2, 2)))
-
+    model.add(tf.keras.layers.Conv2D(64, 3, activation="relu"))
+    model.add(tf.keras.layers.Dropout(0.4))
+    # 62x62 / 2 -> 30x30x64
+    model.add(tf.keras.layers.MaxPooling2D((2, 2)))
     # Finish up by flattening and feeding to a dense layer
     # 63x63 -> 63**2x1
     model.add(tf.keras.layers.Flatten())  # Flattens input matrix
 
-    # NN layer that takes an array of 128 values as input into a 1D array
     model.add(tf.keras.layers.Dense(n_neurons, activation="relu"))
-    # NN layer that takes an array of 129 values as input into a 1D array
-    # model.add(tf.keras.layers.Dense(
-    #    (img_size - 2 // 2)**2,
-    #    activation=tf.nn.relu
-    # ))
+    model.add(tf.keras.layers.Dropout(0.2))
+    model.add(tf.keras.layers.Dense(n_neurons, activation="relu"))
+    model.add(tf.keras.layers.Dropout(0.2))
     model.add(tf.keras.layers.Dense(n_labels, activation="softmax"))
 
     model.compile(
@@ -110,57 +129,48 @@ if __name__ == "__main__":
     x_data, y_data = load_data()
     img_size = x_data.shape[1]
     n_labels = len(np.unique(y_data))
-    # Splits data into kfolds for cross validation
-    """
-    kfold = StratifiedKFold(n_splits=n_folds, shuffle=True)
-    kfold_acc, kfold_loss = [None] * n_folds, [None] * n_folds
-    for i, (train, test) in enumerate(kfold.split(x_data, y_data)):
-        print("Running Fold", i + 1, "/", n_folds)
-        model = None  # Clearing the NN.
-        model = create_model(img_size, n_labels)
-        kfold_loss[i], kfold_acc[i] = train_and_evaluate_model(
-            model, x_data[train], y_data[train], x_data[test], y_data[test]
-        )
-    # Loss and accuracy of each fold
-    plt.bar(range(len(kfold_loss)), height=kfold_loss)
+    # Visualize inputs
+    fig, ax = plt.subplots(5, 5, sharex=True, sharey=True)
+    label_mapping = {0: 'NORMAL', 1: 'INV', 2: 'DEL'}
+    sel_id = np.random.choice(range(x_data.shape[0]), size=25)
+    for i, a in zip(sel_id, ax.flat):
+        a.imshow(np.log1p(x_data[i, :, :]))
+        a.set_title(label_mapping[y_data[i]])
+    plt.suptitle('Random examples of input samples')
     plt.show()
-    plt.bar(range(len(kfold_acc)), height=kfold_acc)
-    plt.show()
-    """
 
     # Compare training and testing performance
-    n_neurons = [12, 15, 20, 30]
+    n_neurons = [60, 70, 80, 90]
     best_acc = 0
     best_n = None
-    """
     for n in n_neurons:
         model = create_model(img_size, n_labels, n_neurons=n)
-        history = model.fit(x_data, y_data, epochs=3, validation_split=0.2)
+        history = model.fit(x_data, y_data, epochs=7, validation_split=0.2)
         acc = history.history["val_accuracy"][-1]
         if acc > best_acc:
             best_acc = acc
             best_n = n
     print(f"Best accuracy is {best_acc}, obtained with {best_n} neurons.")
-    """
-    best_n = 15
 
     model = create_model(img_size, n_labels, n_neurons=best_n)
-    history = model.fit(x_data, y_data, epochs=15, validation_split=0.2)
-    # Plot training & validation accuracy values
-    plt.plot(history.history["accuracy"], label="Train")
-    plt.plot(history.history["val_accuracy"], label="Test")
-    plt.title("Model accuracy")
-    plt.ylabel("Accuracy")
-    plt.xlabel("Epoch")
-    plt.legend(loc="upper left")
-    plt.show()
 
+    print(f'{"-"*10}\nTraining model on {x_data.shape[0]} images of shape {x_data.shape[1]}x{x_data.shape[2]}')
+    history = model.fit(x_data, y_data, epochs=25, validation_split=0.2)
+    print(model.summary())
+
+    # Plot training & validation accuracy values
+    fig, ax = plt.subplots(1, 2)
+    ax[0].plot(history.history["accuracy"], label="Train")
+    ax[0].plot(history.history["val_accuracy"], label="Test")
+    ax[0].set_title("Model accuracy")
+    ax[0].set_ylabel("Accuracy")
+    ax[0].set_xlabel("Epoch")
     # Plot training & validation loss values
-    plt.plot(history.history["loss"], label="Train")
-    plt.plot(history.history["val_loss"], label="Test")
-    plt.title("Model loss")
-    plt.ylabel("Loss")
-    plt.xlabel("Epoch")
+    ax[1].plot(history.history["loss"], label="Train")
+    ax[1].plot(history.history["val_loss"], label="Test")
+    ax[1].set_title("Model loss")
+    ax[1].set_ylabel("Loss")
+    ax[1].set_xlabel("Epoch")
     plt.legend(loc="upper left")
     plt.show()
 
