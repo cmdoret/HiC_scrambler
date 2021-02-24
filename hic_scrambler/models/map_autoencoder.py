19db143c02e9adcedebc10b4288a2a8f9861c99d
cmdoret <cyril.mattheydoret@gmail.com>
2021-02-24 20:10:50 +0100

    finish autoencoder model

--- hic_scrambler/models/map_autoencoder.py
+++ hic_scrambler/models/map_autoencoder.py
@@ -5,6 +5,15 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow.keras.models import Model
 from keras.models import model_from_json
+from tensorflow.keras.layers import (
+    Conv2D,
+    Conv2DTranspose,
+    Dense,
+    Flatten,
+    Input,
+    MaxPool2D,
+    Reshape,
+)
 import glob
 import re
 from os.path import join
@@ -13,36 +22,27 @@ from typing import Tuple
 
 
 def load_data(
-    training_path: str = "data/input/training",
+    training_path: str = "data/input/training_aa", chunk=0,
 ) -> Tuple[np.ndarray, np.ndarray]:
     """Loads training data from several runs, condatenates and normalize it."""
     # Take all sets in training folder
-    x_data = np.sort(
-        [
-            f
-            for f in glob.glob(training_path + "/*")
-            if re.search(r".*x\.npy", f)
-        ]
-    )
-    y_data = np.sort(
-        [
-            f
-            for f in glob.glob(training_path + "/*")
-            if re.search(r".*y\.npy", f)
-        ]
-    )
 
     # Concatenate them into a single array
-    x_data = map(np.load, x_data)
-    y_data = map(np.load, y_data)
-    x_data = np.vstack(list(x_data))
-    y_data = np.hstack(list(y_data))
-    x_data = tf.keras.utils.normalize(x_data, axis=1)
+    x_data = np.load(join(training_path, f"scrambled_chunk_{chunk}.npy"))
+    y_data = np.load(join(training_path, f"truth_chunk_{chunk}.npy"))
+    if x_data.shape != y_data.shape:
+        raise ValueError("Input and output images should have the same shape.")
+
+    # Crop if shape is odd
+    if x_data.shape[1] % 2:
+        x_data = x_data[:, :-1, :-1]
+        y_data = y_data[:, :-1, :-1]
     # Flatten Hi-C images: Divide each image by a diagonal gradient
     # computed across whole dataset
-    x_data = erase_diags(x_data)
+    # x_data = erase_diags(x_data)
     # X must be 4D (for convolutional layers) and made up of floats
     x_data = x_data[:, :, :, None].astype(float)
+    y_data = y_data[:, :, :, None].astype(float)
     return x_data, y_data
 
 
@@ -71,38 +71,37 @@ def erase_diags(imgs: np.ndarray) -> np.ndarray:
 class Unscramble(Model):
     """Autoencoder to generate correct maps from scrambled maps (i.e. with SV)"""
 
-    def __init__(self):
+    def __init__(self, img_size=256):
         super(Unscramble, self).__init__()
+        conv_args = {"kernel_size": 3, "activation": "relu", "padding": "same"}
+        tconv_args = {
+            "kernel_size": 3,
+            "strides": 2,
+            "activation": "relu",
+            "padding": "same",
+        }
         self.encoder = tf.keras.Sequential(
             [
-                tf.layers.Input(shape=(28, 28, 1)),
-                tf.layers.Conv2D(
-                    16, (3, 3), activation="relu", padding="same", strides=2
-                ),
-                tf.layers.Conv2D(
-                    8, (3, 3), activation="relu", padding="same", strides=2
-                ),
+                Input(shape=(img_size, img_size, 1)),
+                Conv2D(16, **conv_args),
+                MaxPool2D((2, 2)),
+                Conv2D(8, **conv_args),
+                MaxPool2D((2, 2)),
+                Conv2D(4, **conv_args),
+                MaxPool2D((2, 2)),
+                Flatten(),
+                Dense(img_size//8 * img_size//8 * 4)
             ]
         )
 
         self.decoder = tf.keras.Sequential(
-            [
-                tf.layers.Conv2DTranspose(
-                    8,
-                    kernel_size=3,
-                    strides=2,
-                    activation="relu",
-                    padding="same",
-                ),
-                tf.layers.Conv2DTranspose(
-                    16,
-                    kernel_size=3,
-                    strides=2,
-                    activation="relu",
-                    padding="same",
-                ),
-                tf.layers.Conv2D(
-                    1, kernel_size=(3, 3), activation="sigmoid", padding="same"
+            [   Dense(img_size//8 * img_size//8 * 4),
+                Reshape((img_size//8, img_size//8, 4)),
+                Conv2DTranspose(4, **tconv_args),
+                Conv2DTranspose(8, **tconv_args),
+                Conv2DTranspose(16, **tconv_args),
+                Conv2D(
+                    1, kernel_size=1, activation="sigmoid", padding="same",
                 ),
             ]
         )
@@ -151,32 +150,40 @@ if __name__ == "__main__":
     # Exploration, parameter search, validation etc
     n_folds = 5
     x_data, y_data = load_data()
+    x_data = x_data[:, :256, :256, :]
+    y_data = y_data[:, :256, :256, :]
+    (x_data, _), (_, _) = tf.keras.datasets.cifar10.load_data()
+    x_data = x_data.sum(axis=3)[:, :, :, None]
+    y_data = x_data + np.random.random((x_data.shape))
     img_size = x_data.shape[1]
-    n_labels = len(np.unique(y_data))
-    test_idx = np.random.choice(range(x_data.shape[0]), size=x_data // 5)
+    # test_idx = np.random.choice(range(x_data.shape[0]), size=x_data // 5)
     # test_mask, train_mask = np.zeros(x_data.shape[0]), np.ones(x_data.shape[0])
     # test_mask[test_idx] = 1
     # train_mask[test_idx] = 0
+    tf.keras.datasets.cifar10.load_data()
 
-    autoencoder = Unscramble()
-    autoencoder.compile(optimizer="adam", loss=tf.losses.MeanSquaredError())
-    autoencoder.fit(
-        x_data, y_data, epochs=10, shuffle=True, validation_split=0.2
-    )
-    autoencoder.encoder.summary()
-    encoded_imgs = autoencoder.encoder(x_data).numpy()
-    decoded_imgs = autoencoder.decoder(encoded_imgs).numpy()
+    autoencoder = Unscramble(img_size=img_size)
+    autoencoder.compile(optimizer="sgd", loss=tf.losses.MeanSquaredError())
     print(
         f'{"-"*10}\nTraining model on {x_data.shape[0]} images of shape {x_data.shape[1]}x{x_data.shape[2]}'
     )
-    print(autoencoder.summary())
+    autoencoder.fit(
+        x_data, y_data, epochs=10, shuffle=True, validation_split=0.2
+    )
+    print(autoencoder.encoder.summary())
+    demo_sample = np.random.choice(range(x_data.shape[0]), size=5)
+    encoded_imgs = x_data[demo_sample]
+    truth_imgs = y_data[demo_sample]
+    decoded_imgs = autoencoder.call(x_data[demo_sample]).numpy()
 
-    # Visualize inputs
-    fig, ax = plt.subplots(5, 5, sharex=True, sharey=True)
-    label_mapping = {0: "NORMAL", 1: "INV", 2: "DEL"}
-    sel_id = np.random.choice(range(x_data.shape[0]), size=25)
-    for i, a in zip(sel_id, ax.flat):
-        a.imshow(np.log1p(x_data[i, :, :]))
-        a.set_title(label_mapping[y_data[i]])
-    plt.suptitle("Random examples of input samples")
+    # Visualize inputs and outputs
+    fig, ax = plt.subplots(5, 3, sharex=True, sharey=True)
+    for i in range(ax.shape[0]):
+        ax[i, 0].imshow(np.log1p(encoded_imgs[i, :, :]))
+        ax[i, 1].imshow(np.log1p(decoded_imgs[i, :, :]))
+        ax[i, 2].imshow(np.log1p(truth_imgs[i, :, :]))
+    plt.suptitle("Random examples of input and decoded samples")
+    ax[0, 0].set_title("Input")
+    ax[0, 1].set_title("Decoded")
+    ax[0, 2].set_title("Truth")
     plt.show()
